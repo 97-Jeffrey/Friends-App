17b35f9f2247d8b4c58745fb9f2f0c1c5bfad047
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2021-02-12 22:18:08 -0800

    the show action of each friend is defined

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,8 +1,20 @@
 class FriendsController < ApplicationController
+  def show
+    @friend = Friend.find(params[:id])
+  end
+  
   def new
   end
 
   def create
-    render plain: params[:friend].inspect
+    # render plain: params[:friend].inspect
+    @friend  = Friend.new(friend_params)
+    @friend.save
+    redirect_to @friend
   end
+
+  private 
+    def friend_params
+      params.require(:friend).permit(:first_name, :last_name, :email, :phone_number,:twitter)
+    end
 end

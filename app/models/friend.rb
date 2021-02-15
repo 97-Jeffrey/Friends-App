088930f5098e562391fc03cbfa71a364925c32a1
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2021-02-14 19:12:32 -0800

    association is added between user and friend

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,6 +1,5 @@
 class Friend < ApplicationRecord
   validates :first_name, presence:true
-  # validates :last_name, presence:true,
-  # validates :phone_number, presence:true, length:{ minimum: 3}
+  belongs_to :user
 
 end
